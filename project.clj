defcb721cb00d4ef5eb80dd7c57d27ee2b66ad59
Gunnar Völkel <Gunnar.Voelkel@uni-ulm.de>
2011-10-12 13:15:55 +0200

    License information and README added.

--- project.clj
+++ project.clj
@@ -1,5 +1,11 @@
 (defproject lein-checkouts "1.0.0-SNAPSHOT"
   	:description "Executes the given lein task on all checkout dependencies of the current project."
+	:license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
+
+
   	:dev-dependencies [[org.clojure/clojure "1.2.1"]]
 	:eval-in-leiningen true
 )
