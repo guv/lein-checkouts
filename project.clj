3b6f70c66f72f640b6a6f0484ab3f8984b722cc4
Gunnar Völkel <Gunnar.Voelkel@uni-ulm.de>
2011-10-12 13:48:07 +0200

    README upgrade.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-checkouts "1.0.0-SNAPSHOT"
+(defproject lein-checkouts "1.0.0"
   	:description "Executes the given lein task on all checkout dependencies of the current project."
 	:license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
@@ -6,6 +6,6 @@
             :comments "same as Clojure"}
 
 
-  	:dev-dependencies [[org.clojure/clojure "1.2.1"]]
+  	:dev-dependencies [[org.clojure/clojure "1.2.1"] [lein-clojars "0.7.0"]]
 	:eval-in-leiningen true
 )
