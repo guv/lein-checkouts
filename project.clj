60963529a0c6ede2020af7c2ff445bfea1fb930f
Gunnar Völkel <Gunnar.Voelkel@googlemail.com>
2013-01-13 20:03:41 +0100

    Version 1.1.0.

--- project.clj
+++ project.clj
@@ -1,21 +1,10 @@
-(defproject lein-checkouts "1.0.0"
+(defproject lein-checkouts "1.1.0"
   	:description "Executes the given lein task on all checkout dependencies of the current project."
 	:license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
 
-  :profiles {
-    :dev {
-      :dependencies [
-                      [org.clojure/clojure "1.4.0"]
-                    ]
-      :plugins [[lein-clojars "0.9.1"]]
-    }
-
-
-
-  }
-  
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]]}}
   :eval-in-leiningen true
 )
